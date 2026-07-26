6994ccc3429f92a1366cfb95a036b209726757ac
Simon Sørensen <61250140+zarstensen@users.noreply.github.com>
2026-07-26 18:53:54 +0200

    sync

---
+++ .nvim.lua
@@ -0,0 +1,19 @@
+vim.schedule(function()
+	local deb = require("utils.debounce"):new(1000)
+
+	local ovs = require("overseer")
+
+	local install_packs_task = ovs.new_task({
+		cmd = "chezmoi",
+		args = {"apply"},
+		name = "Chezmoi Apply",
+	})
+	install_packs_task:inc_reference()
+
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		pattern = ".chezmoidata/packages/*.yaml",
+		callback = deb:debounced(function(_)
+			install_packs_task:restart()
+		end),
+	})
+end)
---
+++ list-hosttype-pkgs.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import json
+import subprocess
+import sys
+
+HOSTTYPE_CATEGORIES = {
+    "desktop": [
+        "pkgs_common",
+        "pkgs_fonts",
+        "pkgs_music",
+        "pkgs_dev",
+        "pkgs_nvim",
+        "pkgs_gaming",
+    ],
+    "laptop": [
+        "pkgs_common",
+        "pkgs_laptop",
+        "pkgs_fonts",
+        "pkgs_music",
+        "pkgs_dev",
+        "pkgs_nvim",
+    ],
+}
+
+
+def main():
+    assert len(sys.argv) == 2, f"Missing hosttype arg, must be one of {HOSTTYPE_CATEGORIES.keys()}"
+    role = sys.argv[1]
+    categories = HOSTTYPE_CATEGORIES[role]
+
+    # get the packages from chezmoi, so we let chezmoi parse them as yaml for us essentially.
+    data = json.loads(subprocess.check_output(["chezmoi", "data", "--format=json"]))
+
+    for cat in categories:
+        for pkg in data.get(cat, []):
+            print(pkg)
+
+
+if __name__ == "__main__":
+    main()
