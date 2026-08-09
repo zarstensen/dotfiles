0ce1721a19d074e3b1f0b73ab3cca3385c7059cd
Simon Sørensen <61250140+zarstensen@users.noreply.github.com>
2026-08-09 21:32:38 +0200

    fix neogit and ghosttext incompatiblity

--- dotfiles/dot_config/nvim/plugin/editor/neogit.lua
+++ dotfiles/dot_config/nvim/plugin/editor/neogit.lua
@@ -5,5 +5,17 @@ vim.pack.add({
 	"https://github.com/neogitorg/neogit",
 })
 
+-- override process spawn so NVIM_LISTEN_ADDRESS is not set to anything,
+-- some other plugins may set this (GhostText) which breaks neogit.
+local Process = require("neogit.process")
+
+local orig_spawn = Process.spawn
+
+function Process:spawn(cb)
+	self.env = self.env or {}
+	self.env.NVIM_LISTEN_ADDRESS = ""
+	return orig_spawn(self, cb)
+end
+
 vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", { desc = "Open Neo[g]it" })
 vim.api.nvim_create_user_command("Git", "Neogit", { nargs = 0 })
