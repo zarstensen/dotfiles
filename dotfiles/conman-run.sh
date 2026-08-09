472572b4e773817e8a740be119f373414314f3ce
Simon Sørensen <61250140+zarstensen@users.noreply.github.com>
2026-08-09 22:16:05 +0200

    more grep fixes

--- dotfiles/conman-run.sh
+++ dotfiles/conman-run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eufo pipefail
 
+if command -v grep >/dev/null 2>&1; then
+    sudo pacman -S grep
+fi
+
 sudo pacman -S --needed --noconfirm python python-yaml python-pydantic python-bracex git >/dev/null 2> >(grep --line-buffered -v 'is up to date -- skipping')
 
 cd "$CHEZMOI_WORKING_TREE"
