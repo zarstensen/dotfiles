5056737e2571912fbead35d65ab12430c2714941
Simon Sørensen <61250140+zarstensen@users.noreply.github.com>
2026-08-10 19:41:58 +0200

    fix tuned services placement

--- conman/scripts/services/run_after_common.sh
+++ conman/scripts/services/run_after_common.sh
@@ -2,5 +2,3 @@
 set -eufo pipefail
 
 sudo systemctl enable --now earlyoom
-sudo systemctl enable --now tuned
-sudo systemctl enable --now tuned-ppd
--- conman/scripts/services/run_after_laptop.sh
+++ conman/scripts/services/run_after_laptop.sh
@@ -2,3 +2,5 @@
 set -eufo pipefail
 
 sudo systemctl enable --now keyd
+sudo systemctl enable --now tuned
+sudo systemctl enable --now tuned-ppd
