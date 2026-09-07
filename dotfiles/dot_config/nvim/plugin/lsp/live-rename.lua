84fc7f34f1f2869b96aef92a2a96dd45d14ae120
Simon Sørensen <61250140+zarstensen@users.noreply.github.com>
2026-09-07 22:24:33 +0200

    rename symbol fallback

--- dotfiles/dot_config/nvim/plugin/lsp/live-rename.lua
+++ dotfiles/dot_config/nvim/plugin/lsp/live-rename.lua
@@ -5,4 +5,12 @@ vim.pack.add({
 local lr = require("live-rename")
 lr.setup({})
 
-vim.keymap.set({ "n", "x" }, "<leader>nr", lr.map({ cursorpos = 0 }), { desc = "[R]ename Symbol" })
+vim.keymap.set({ "n", "x" }, "<leader>nr", function()
+	local clients_rename_support = vim.lsp.get_clients({bufnr = 0, method = vim.lsp.protocol.Methods.textDocument_rename})
+	if not vim.tbl_isempty(clients_rename_support) then
+		lr.rename({ cursorpos = 0 })
+	else
+		-- fallback to search and replace if an lsp does not exist which supports renaming
+		vim.api.nvim_feedkeys("*:%s//", "n ", false)
+	end
+end, { desc = "[R]ename Symbol" })
