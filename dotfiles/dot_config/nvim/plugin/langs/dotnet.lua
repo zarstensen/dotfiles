11696ac423f067c54d67e01ae94b559eecc85450
Simon Sørensen <61250140+zarstensen@users.noreply.github.com>
2026-09-10 08:38:50 +0200

    dotfiles

--- dotfiles/dot_config/nvim/plugin/langs/dotnet.lua
+++ dotfiles/dot_config/nvim/plugin/langs/dotnet.lua
@@ -2,6 +2,12 @@ vim.pack.add({
 	"https://github.com/nvim-lua/plenary.nvim",
 	"https://github.com/folke/snacks.nvim",
 	"https://github.com/GustavEikaas/easy-dotnet.nvim.git",
+
+	-- solution explorer
+	"https://github.com/nvim-tree/nvim-tree.lua",
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/dtrh95/csharp-explorer.nvim",
 })
 
 require("easy-dotnet").setup({ picker = "fzf" })
+require("csharp-explorer").setup({})
